8fe009954e35866740fb9b722355c174a0fe3953
MounaSafiHarab <mouna.safiharab@gmail.com>
2016-11-07 16:37:38 -0500

    Phantoms for CCNA; take 2

--- uploadNeuroDB/NeuroDB/ImagingUpload.pm
+++ uploadNeuroDB/NeuroDB/ImagingUpload.pm
@@ -196,11 +196,11 @@ sub IsCandidateInfoValid {
          #Validate the Patient-Name, only if it's not a phantom#
          ############## and the file is of type DICOM###########
          #######################################################
-                if ($row[4] eq 'N') {
+              #  if ($row[4] eq 'N') {
                     if ( !$this->PatientNameMatch($_) ) {
                             $files_with_unmatched_patient_name++;
                     }
-                }
+             #   }
             }
         }
     }
